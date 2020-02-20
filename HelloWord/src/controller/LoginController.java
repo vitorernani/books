8d75c45ed480c1cf4f6376503bd0eabd42589479
Vitor Ernani <Aluno@PAT1727>
2020-02-20 10:59:22 -0300

    20/02

---
+++ HelloWord/src/controller/LoginController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+	
+	private String login;
+	private String senha;
+	
+	public String entrar() {
+		if(login.equals("joao") && senha.equals("123")) {
+			//redirecionar
+			return "helloword.xhtml";	
+		}
+		//if der errado mandar mensagem
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+				"Login ou Senha Invalido.", null));
+		
+		return null;
+	}
+	public void limpar() {
+		login = "";
+		senha = "";
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
