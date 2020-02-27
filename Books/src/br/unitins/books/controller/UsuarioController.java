5c5b7faf79dd3d599d4b97c9fc6dfe64c17f6574
Vitor Ernani <Aluno@PAT1727>
2020-02-27 09:54:18 -0300

    att

--- Books/src/br/unitins/books/controller/UsuarioController.java
+++ Books/src/br/unitins/books/controller/UsuarioController.java
@@ -5,14 +5,29 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import br.unitins.books.models.Usuario;
+
 @Named
 @ViewScoped
 public class UsuarioController implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -563227102506849534L;
 
+	private Usuario usuario;
+	
+	public void incluir(){
+		System.out.println(getUsuario().getNome());
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
-//dsa
--- Books/src/br/unitins/books/models/Usuario.java
+++ Books/src/br/unitins/books/models/Usuario.java
@@ -7,4 +7,37 @@ public class Usuario {
 	private String login;
 	private String senha;
 	private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 }
